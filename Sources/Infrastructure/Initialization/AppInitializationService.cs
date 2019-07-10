303237fe3433a5366baf93c6a05abcf30e731da9
Matthias Müller <matthias.mueller@trivadis.com>
2019-07-10 07:57:09 +0200

    WIP

--- Sources/Infrastructure/Initialization/AppInitializationService.cs
+++ Sources/Infrastructure/Initialization/AppInitializationService.cs
@@ -19,7 +19,7 @@ namespace Mmu.Mls3.WebApi.Infrastructure.Initialization
 
             app.UseHttpsRedirection();
             app.UseMiddleware<ErrorHandlingMiddleware>();
-            app.UseMiddleware<DbMigrationMiddleware>();
+            ////app.UseMiddleware<DbMigrationMiddleware>();
             app.UseCors("All");
             app.UseHttpsRedirection();
             app.UseAuthentication();
