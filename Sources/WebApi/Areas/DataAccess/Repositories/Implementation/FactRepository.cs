b017a4667a1ad7da54d60c380e9efa12af515f36
Matthias Müller <matthias.mueller@trivadis.com>
2019-08-21 15:21:47 +0200

    WIP

--- Sources/WebApi/Areas/DataAccess/Repositories/Implementation/FactRepository.cs
+++ Sources/WebApi/Areas/DataAccess/Repositories/Implementation/FactRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Mmu.Mls3.WebApi.Areas.DataAccess.Entities;
 using Mmu.Mls3.WebApi.Infrastructure.DataAccess.Repositories.Base;
 using Mmu.Mls3.WebApi.Infrastructure.DataAccess.Repositories.Servants;
@@ -17,6 +18,11 @@ namespace Mmu.Mls3.WebApi.Areas.DataAccess.Repositories.Implementation
         public Task<IReadOnlyCollection<Fact>> LoadByIdsAsync(IReadOnlyCollection<long> factIds)
         {
             return LoadAsync(f => factIds.Contains(f.Id.Value));
+        }
+
+        protected override IQueryable<Fact> AppendIncludes(IQueryable<Fact> query)
+        {
+            return query.Include(f => f.LearningSessionFacts);
         }
     }
 }
--- Sources/WebApi/Areas/Web/Dtos/Profiles/FactOverviewEntryDtoProfile.cs
+++ Sources/WebApi/Areas/Web/Dtos/Profiles/FactOverviewEntryDtoProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Linq;
+using AutoMapper;
 using Mmu.Mls3.WebApi.Areas.DataAccess.Entities;
 
 namespace Mmu.Mls3.WebApi.Areas.Web.Dtos.Profiles
@@ -10,6 +11,7 @@ namespace Mmu.Mls3.WebApi.Areas.Web.Dtos.Profiles
             CreateMap<Fact, FactOverviewEntryDto>()
                 .ForMember(d => d.CreationDateDescription, c => c.MapFrom(f => f.CreationDate.ToString("dd.MM.yyyy hh:mm:ss")))
                 .ForMember(d => d.Id, c => c.MapFrom(f => f.Id))
+                .ForMember(d => d.ExistsInRun, c => c.MapFrom(f => f.LearningSessionFacts.Any()))
                 .ForMember(d => d.QuestionText, c => c.MapFrom(f => f.QuestionText));
         }
     }
